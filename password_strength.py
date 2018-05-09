8aae9f6313789b07c2582ba75dfeba7cb3444607
svobodinang <svobodinang@mail.ru>
2018-05-09 22:11:04 +0300

    delete backslash

--- password_strength.py
+++ password_strength.py
@@ -35,8 +35,9 @@ def get_password_strength(password, personal_data, filepath):
     complexity = 0
     error_list = []
     last_name, first_name, patronymic, company_name = personal_data
-    personal_information_present, company_name_present = \
+    personal_information_present, company_name_present = (
         has_not_personal_information(password, personal_data)
+    )
     black_list = load_blacklist(filepath)
 
     all_checks_password = [
@@ -67,7 +68,7 @@ def get_password_strength(password, personal_data, filepath):
 
 def output_complexity(complexity, error_indexes_list):
     print("Сложность вашего пароль: ", complexity)
-    names_error = [
+    errors = [
         "Пароль должен состоять более чем из 8 символов",
         "Испульзуйте буквы разного регистра",
         "Используйте не только буквы, но и цифры",
@@ -81,24 +82,26 @@ def output_complexity(complexity, error_indexes_list):
         "Не используйте номерные знаки в пароле",
     ]
     for index in error_indexes_list:
-        print(names_error[index])
+        print(errors[index])
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        exit("Вы не ввели путь к файлу с самыми полпулярными паролями")
+        exit("Вы не ввели путь к файлу с самыми популярными паролями")
     file_path = sys.argv[1]
     if not os.path.isfile(file_path):
         exit("Такого файла не существует")
 
-    last_name, first_name, patronymic, company_name = \
+    last_name, first_name, patronymic, company_name = (
         user_input_personal_data()
+    )
     if not last_name or not first_name:
         exit("Вы ввели не все персональные данные")
     password = user_input_password()
     if password == "":
         exit("Вы не ввели пароль")
     personal_data = last_name, first_name, patronymic, company_name
-    complexity, error_list = \
+    complexity, error_list = (
         get_password_strength(password, personal_data, file_path)
+    )
     output_complexity(complexity, error_list)
